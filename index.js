cba3a2fa8e26c18d965277b06b07b6e1716714aa
navya-22101999 <navyatammanaboina@gmail.com>
2023-08-14 05:47:01 +0000

    cd1

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="main-container">
+    <h1>Super Over League</h1>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        alt="rcb"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        alt="csk"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
